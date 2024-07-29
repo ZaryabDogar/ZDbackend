9675f9c6a86c138d30dfca9195647071bd5a2627
zaryabdogar <zaryabdogar23@gmail.com>
2024-07-29 11:40:21 +0500

    mongo db

--- db.js
+++ db.js
@@ -1,25 +1,18 @@
 require('dotenv').config();
-
-
-const mongouri = process.env.MONGO_URI || "mongodb+srv://zaryabdogar23:dogar1234@cluster0.8xpxs3y.mongodb.net/zdfood?retryWrites=true&w=majority&appName=Cluster0"
-
-
 const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://zaryabdogar23:dogar1234@cluster0.8xpxs3y.mongodb.net/zdfood?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(mongouri, {
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      
     });
-
     console.log('Connected to MongoDB successfully');
-    const fetched_data= mongoose.connection.db.collection("food_data");
-    const data=await fetched_data.find({}).toArray()
-    // console.log(data);
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
+    throw new Error('MongoDB connection failed');
   }
 };
 
